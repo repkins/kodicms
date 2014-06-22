da79e4ad04b662ea3f0e09d43b313078cb0f59f0
Warren E. White <wwhite@nerret-tech.com>
2014-06-22 11:29:35 -0700

    Fixed white space formatting.

--- cms/plugins/maintenance/frontend.php
+++ cms/plugins/maintenance/frontend.php
@@ -18,7 +18,7 @@ if($plugin->get('maintenance_mode') == Config::YES)
 			$page = Model_Page_Front::find( $page->slug );
 
 			// if we fund it, display it!
-			if( is_object($page) )
+			if (is_object($page))
 			{
 				echo Response::factory()
 					->status(403)
@@ -29,7 +29,7 @@ if($plugin->get('maintenance_mode') == Config::YES)
 		} 
 		else 
 		{
-			throw new HTTP_Exception_403( 'Maintenance mode' );
+			throw new HTTP_Exception_403('Maintenance mode');
 			exit();
 		}
 	});
