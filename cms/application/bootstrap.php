7323f90db069087ed1527600bc734498cf4d8e65
root <root@184731-20001.vm.clodoserver.ru>
2014-06-30 08:36:53 +0000

    update

--- cms/application/bootstrap.php
+++ cms/application/bootstrap.php
@@ -38,7 +38,7 @@ setlocale( LC_ALL, 'en_US.utf-8' );
  * @link http://kohanaframework.org/guide/using.autoloading
  * @link http://www.php.net/manual/function.spl-autoload-register
  */
-//spl_autoload_register(array('Kohana', 'auto_load'));
+spl_autoload_register(array('Kohana', 'auto_load'));
 
 /**
  * Optionally, you can enable a compatibility auto-loader for use with
@@ -46,7 +46,7 @@ setlocale( LC_ALL, 'en_US.utf-8' );
  *
  * It is recommended to not enable this unless absolutely necessary.
  */
-spl_autoload_register(array('Kohana', 'auto_load_lowercase'));
+//spl_autoload_register(array('Kohana', 'auto_load_lowercase'));
 
 /**
  * Enable the Kohana auto-loader for unserialization.
